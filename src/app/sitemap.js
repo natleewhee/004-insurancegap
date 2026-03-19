9c94cfc2b39e01b312635ec97b43ed6ad67f5ffb
nathaniel <natleewhee@gmail.com>
2026-03-19 22:36:35 +0800

    mvp2

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,22 @@
 export default function sitemap() {
-    return [
-      {
-        url: 'https://insurecheck.sg',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 1,
-      },
-      {
-        url: 'https://insurecheck.sg/how-it-works',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://insurecheck.sg/check',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.9,
-      },
-    ]
-  }
+  return [
+    {
+      url: 'https://sginsurecheck.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://sginsurecheck.vercel.app/how-it-works',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sginsurecheck.vercel.app/check',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+  ]
+}
